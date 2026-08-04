3385a86d62aacabc89421c24b9f949da30458deb
Dhruvkittur <145898159+Dhruvkittur@users.noreply.github.com>
2026-08-04 15:30:29 +0530

    Fix import statement for EnsembleRetriever

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ from langchain_community.utilities import ArxivAPIWrapper
 from langchain_community.vectorstores import FAISS
 from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_community.retrievers import BM25Retriever
-from langchain.retrievers import EnsembleRetriever
+from langchain_community.retrievers import EnsembleRetriever
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_core.messages import BaseMessage, HumanMessage
 from langchain_core.documents import Document
